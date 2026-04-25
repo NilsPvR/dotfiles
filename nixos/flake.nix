7b47a50b219e1d92bfed931d62114cb1fb38dc3b
NilsPvR <nilspvr@pm.me>
2026-04-25 13:10:20 +0200

    build: use flake parts
    not really using them rn but we'll get there

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,21 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { nixpkgs, ... } @ inputs:
-  let
-  
-  in
-  {
-    nixosConfigurations = {
-      # This should correspond to the hostname
-      nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./configuration.nix
-        ];
+  outputs = { flake-parts, nixpkgs, ... } @ inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } ({ config, withSytem, moduleWithSystem, ... } @ top: {
+      imports = [
+        # Use external flake logic
+      ];
+      flake = {
+        # Normale flake attributes
+        nixosConfigurations = {
+          # This should correspond to the hostname
+          nixos = nixpkgs.lib.nixosSystem {
+            specialArgs = { inherit inputs; };
+            modules = [
+              ./configuration.nix
+            ];
+          };
+        };
       };
-    };
-  };
+      systems = [
+        # Systems for which to build `perSystem` attribute
+        "x86_64-linux"
+      ];
+      perSystem = { pkgs, ...}: {
+        
+      };
+    });
 }
